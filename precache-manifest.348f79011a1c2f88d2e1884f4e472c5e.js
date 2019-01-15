3e3b326f44e63d5b326804a5426ec849ffa267ee
TheNoodleMoose <cjh605@gmail.com>
2019-01-15 18:14:38 -0500

    Updates

---
+++ precache-manifest.348f79011a1c2f88d2e1884f4e472c5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61aaf43f18ba1f2b46de",
+    "url": "/React-Memory-Match/static/js/runtime~main.61aaf43f.js"
+  },
+  {
+    "revision": "3c906fb348c1cd3bbacc",
+    "url": "/React-Memory-Match/static/js/main.3c906fb3.chunk.js"
+  },
+  {
+    "revision": "552d65ec4fc073a48a59",
+    "url": "/React-Memory-Match/static/js/1.552d65ec.chunk.js"
+  },
+  {
+    "revision": "3c906fb348c1cd3bbacc",
+    "url": "/React-Memory-Match/static/css/main.8bab98bd.chunk.css"
+  },
+  {
+    "revision": "7bad394907c674556a19a193a108d0f7",
+    "url": "/React-Memory-Match/index.html"
+  }
+];
